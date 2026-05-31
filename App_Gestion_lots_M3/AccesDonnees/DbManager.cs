631fbb0c68add3e5b31f2122d43822b0cdd3cf02
MathyCH <94693019+MathildeMertz@users.noreply.github.com>
2026-05-31 21:32:57 +0200

    Ajout de la connection à la DB de Docker
    ensemble des fonctions permettant de se conencter a la db et de vérifier la connexion

---
+++ App_Gestion_lots_M3/AccesDonnees/DbManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace App_Gestion_lots_M3.AccesDonnees
+{
+    internal class DbManager
+    {
+        static private MySqlConnection _connection;
+
+        public static void ConnectToDB(string databaseName, string userName, string password, string host = "localhost", int port = 3306)
+        {
+            string connectionString =
+                $"server={host};database={databaseName};user={userName};password={password};port={port}";
+
+            _connection = new MySqlConnection(connectionString);
+            _connection.Open();
+        }
+
+        public static void CloseDBConnection()
+        {
+            if (_connection != null)
+                _connection.Close();
+        }
+
+        public static MySqlConnection GetDBConnection()
+        {
+            return _connection;
+        }
+
+        public static bool IsConnected()
+        {
+            return _connection != null && _connection.State == System.Data.ConnectionState.Open;
+        }
+
+
+    }
+}
